a6c04c107f2de1bc0b5ea739820fdebb0fd0e349
noam finkelstein <noam.finkelstein@gmail.com>
2017-01-31 17:34:27 -0500

    Update anaconda version

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -1,2 +1,3 @@
-wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh 
-bash Anaconda3-4.2.0-Linux-x86_64.sh
+rm -rf ~/code/anaconda
+wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh 
+bash Anaconda3-4.3.0-Linux-x86_64.sh

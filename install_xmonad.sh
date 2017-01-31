3464532cca89f8adc2b00a0633dc6d44da8d20d6
noam finkelstein <noam.finkelstein@gmail.com>
2017-01-31 17:56:16 -0500

    Remove existing xmonad desktop config

--- install_xmonad.sh
+++ install_xmonad.sh
@@ -1,3 +1,4 @@
+sudo rm -f /usr/share/xsessions/xmonad.desktop
 sudo apt install xcape
 sudo apt install xcompmgr
 sudo apt install xmonad
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 ./install_git.sh
+./install_xmonad.sh
 sudo ./install_dotfiles.sh
 ./install_python.sh
 ./install_skype.sh
 ./clone_repos.sh
 ./install_emacs.sh
 ./install_elixir.sh
-./install_xmonad.sh
 mkdir downloads

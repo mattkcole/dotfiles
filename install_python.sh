dc577961eb15e436ba45720b76d8d2b1b1b384ed
noam <noam.finkelstein@gmail.com>
2017-02-05 16:18:45 -0500

    Update python installation for pystan

--- install_python.sh
+++ install_python.sh
@@ -1,3 +1,13 @@
 rm -rf ~/code/anaconda
+rm -rf ~/code/anaconda_pystan
+rm -rf ~/code/anaconda_python2
+
+wget https://repo.continuum.io/archive/Anaconda3-4.1.0-Linux-x86_64.sh 
+bash Anaconda3-4.1.0-Linux-x86_64.sh
+
 wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh 
 bash Anaconda3-4.3.0-Linux-x86_64.sh
+
+wget https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh 
+bash Anaconda2-4.3.0-Linux-x86_64.sh
+

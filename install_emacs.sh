b5886cee7ff40f559c3af459f2f42d845a5c57b2
noam finkelstein <noam.finkelstein@gmail.com>
2017-02-15 10:14:17 -0500

    Remove default emacs config during installation

--- install_emacs.sh
+++ install_emacs.sh
@@ -1,6 +1,6 @@
-rm -rf ~/.emacs.d
 sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt-get update
 sudo apt-get install emacs25
+rm -rf ~/.emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 

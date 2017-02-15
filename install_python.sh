928ac21ac0026b1ca78e89dc88d56cfc791e5b7c
noam finkelstein <noam.finkelstein@gmail.com>
2017-02-15 09:04:30 -0500

    Reorder for consistency

--- install_python.sh
+++ install_python.sh
@@ -2,8 +2,8 @@ wget https://repo.continuum.io/archive/Anaconda3-4.1.0-Linux-x86_64.sh
 wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh 
 wget https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh 
 
-rm -rf ~/code/anaconda
 rm -rf ~/code/anaconda_pystan
+rm -rf ~/code/anaconda
 rm -rf ~/code/anaconda_python2
 
 bash Anaconda3-4.1.0-Linux-x86_64.sh

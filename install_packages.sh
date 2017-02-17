d04641c6ebf958233b07fa3ec8cc719f0b855d6f
noam finkelstein <noam.finkelstein@gmail.com>
2017-02-17 10:21:14 -0500

    Latex ubuntu packages

--- install_packages.sh
+++ install_packages.sh
@@ -4,4 +4,9 @@ sudo apt install xbacklight
 sudo apt install htop
 sudo apt install r-base-core
 sudo apt install gitk
+sudo apt install pandoc
+sudo apt install texlive-latex-base
+sudo apt install texlive-fonts-recommended
+sudo apt install texlive-fonts-extra
+sudo apt install texlive-latex-extra
 

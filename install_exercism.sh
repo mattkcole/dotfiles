015538a451494048bef815e3f5f13dda8be80a2e
noam finkelstein <noam.finkelstein@gmail.com>
2017-03-05 14:13:42 -0500

    Add exercism configuration

--- install_exercism.sh
+++ install_exercism.sh
@@ -1,3 +1,6 @@
 wget https://github.com/exercism/cli/releases/download/v2.3.0/exercism-linux-64bit.tgz
 tar -xzvf exercism-linux-64bit.tgz
 sudo mv exercism /bin/
+exercism configure --key=3354d72143984a519163681fc06c123c
+exercism configure --dir=~/code/exercism
+
